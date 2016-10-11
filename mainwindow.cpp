f54f9b29aca3431025b2a1ea959a61b00215aef6
Deepthi Naidu <deepthinaidu@Deepthis-MacBook-Air.local>
2016-10-11 17:23:16 -0400

    username and password for login screen

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,7 +5,6 @@
 MainWindow::~MainWindow(){
     delete m_p_create_button;
     delete m_p_login_button;
-
 };
 
 MainWindow::MainWindow(QWidget *parent)
@@ -14,8 +13,30 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
     // Create the button, make "this" the parent
+    m_p_login_button = new QPushButton("Sign In", this);
     m_p_create_button = new QPushButton("Create Account", this);
-    m_p_login_button = new QPushButton("Login", this);
+
+    name = new QLabel(this);
+    password = new QLabel(this);
+
+    text_name = new QLineEdit(this);
+    text_pswd = new QLineEdit(this);
+
+
+    text_name->setGeometry(QRect(QPoint(500, 315), QSize(200, 25)));
+    text_pswd->setGeometry(QRect(QPoint(500, 390), QSize(200, 25)));
+
+
+
+
+    name->setText("Username:");
+    name->setGeometry(QRect(QPoint(400,275), QSize(200, 100)));
+    password->setText("Password:");
+    password->setGeometry(QRect(QPoint(400, 375), QSize(400, 50)));
+
+
+
+    //QTextEdit *txt = new QTextEdit();
 
     //  to set size and location of the button
     m_p_create_button->setGeometry(QRect(QPoint(650, 500), QSize(200, 50)));
@@ -39,6 +60,9 @@ void MainWindow::handleButton()
     m_p_login_button->resize(200,50);
 
 }
+
+
+
 
 
 
