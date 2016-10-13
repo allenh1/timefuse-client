bd61c1ef8dc98b01331079b02162ca1676705b1c
Hunter L. Allen <allen286@purdue.edu>
2016-10-13 10:43:42 -0400

    Fixed naming consistency and file modes. Also removed extraneous files.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,54 +1,43 @@
 #include "mainwindow.hpp"
 
-
-//destructor
-MainWindow::~MainWindow(){
-    delete m_p_create_button;
-    delete m_p_login_button;
-};
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 
 {
+	m_p_login_button = new QPushButton(tr("Sign In"), this);
+	m_p_create_button = new QPushButton(tr("Create Account"), this);
 
-    // Create the button, make "this" the parent
-    m_p_login_button = new QPushButton("Sign In", this);
-    m_p_create_button = new QPushButton("Create Account", this);
+    m_p_user_label = new QLabel(tr("Username:"), this);
+    m_p_password_label = new QLabel(tr("Password:"), this);
 
-    name = new QLabel(this);
-    password = new QLabel(this);
+    m_p_user_edit = new QLineEdit(this);
+    m_p_password_edit = new QLineEdit(this);
 
-    text_name = new QLineEdit(this);
-    text_pswd = new QLineEdit(this);
+    m_p_user_edit->setGeometry(QRect(QPoint(500, 315), QSize(200, 25)));
+    m_p_password_edit->setGeometry(QRect(QPoint(500, 390), QSize(200, 25)));
 
+    m_p_user_label->setText("Username:");
+    m_p_user_label->setGeometry(QRect(QPoint(400,275), QSize(200, 100)));
+    m_p_password_label->setText("Password:");
+    m_p_password_label->setGeometry(QRect(QPoint(400, 375), QSize(400, 50)));
 
-    text_name->setGeometry(QRect(QPoint(500, 315), QSize(200, 25)));
-    text_pswd->setGeometry(QRect(QPoint(500, 390), QSize(200, 25)));
-
-
-
-
-    name->setText("Username:");
-    name->setGeometry(QRect(QPoint(400,275), QSize(200, 100)));
-    password->setText("Password:");
-    password->setGeometry(QRect(QPoint(400, 375), QSize(400, 50)));
-
-
-
-    //QTextEdit *txt = new QTextEdit();
-
-    //  to set size and location of the button
     m_p_create_button->setGeometry(QRect(QPoint(650, 500), QSize(200, 50)));
     m_p_login_button->setGeometry(QRect(QPoint(650, 550), QSize(200, 50)));
-
 
     // Connect button signal to appropriate slot
     connect(m_p_create_button, &QPushButton::released, this, &MainWindow::handleButton);
     connect(m_p_login_button, &QPushButton::released, this, &MainWindow::handleButton);
-
-
 }
+
+MainWindow::~MainWindow()
+{
+    delete m_p_create_button;
+    delete m_p_login_button;
+	delete m_p_user_label;
+	delete m_p_password_label;
+	delete m_p_user_edit;
+	delete m_p_password_edit;
+};
 
 void MainWindow::handleButton()
 {
@@ -58,7 +47,6 @@ void MainWindow::handleButton()
     // to resize buttons on clicking
     m_p_create_button->resize(200,50);
     m_p_login_button->resize(200,50);
-
 }
 
 
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -1,48 +1,34 @@
-#ifndef MAINWINDOW_HPP
-#define MAINWINDOW_HPP
-
+#ifndef __MAINWINDOW_HPP__
+#define __MAINWINDOW_HPP__
+#include <QApplication>
 #include <QMainWindow>
 #include <QPushButton>
 #include <QLineEdit>
 #include <QLabel>
-#include <QApplication>
-#include <QObject>
-#include <QTextFrame>
-#include <QtGui>
 #include <QtCore>
-#include <QGridLayout>
-#include <QTextEdit>
+#include <QtGui>
 
 
-namespace Ui {
-    class MainWindow;
-
-
-}
+namespace Ui { class MainWindow; }
 
 class MainWindow : public QMainWindow
 {
-
-
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     virtual ~MainWindow();
-    QLabel *name;
-    QLabel *password;
-    QLineEdit *text_name;
-    QLineEdit *text_pswd;
 
 private:
-    QPushButton *m_p_create_button; //create account
-    QPushButton *m_p_login_button; //login
+    QPushButton *m_p_create_button;
+    QPushButton *m_p_login_button;
 
+	QLabel * m_p_user_label;
+	QLabel * m_p_password_label;
 
+	QLineEdit * m_p_user_edit;
+	QLineEdit * m_p_password_edit;
 
     Q_SLOT void handleButton();
-
 };
-
-
 #endif // MAINWINDOW_H
