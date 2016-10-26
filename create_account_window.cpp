047b9eafde068a2cc8c3d8ad77ac81d7976525a1
Gunjan Gauri <ggauri@purdue.edu>
2016-10-26 19:02:34 -0400

    create_account_window_screen- UI

--- create_account_window.cpp
+++ create_account_window.cpp
@@ -1,5 +1,17 @@
 #include "create_account_window.hpp"
+#include "ui_create_account_window.h"
 
 create_account_window::~create_account_window()
 { /* free things */ }
 
+/*create_account_window::create_account_window(QWidget *parent) :
+QWidget(parent),
+ui(new Ui::create_account_window)
+{
+    ui->setupUi(this);
+}
+
+create_account_window::~create_account_window()
+{
+    delete ui;
+}*/
