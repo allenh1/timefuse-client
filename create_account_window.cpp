fbb3b776a3df2d41b5f2b5a2057162c1d108b943
Deepthi Naidu <naidud@pal-nat186-123-182.itap.purdue.edu>
2016-10-27 15:50:06 -0400

    Always check the .pro file\!

--- create_account_window.cpp
+++ create_account_window.cpp
@@ -1,17 +1,13 @@
 #include "create_account_window.hpp"
-#include "ui_create_account_window.h"
-
-/*create_account_window::~create_account_window()
-{ /* free things */ }*/
 
 create_account_window::create_account_window(QWidget *parent) :
-QWidget(parent),
-ui(new Ui::create_account_window)
+    QWidget(parent),
+    m_p_ui(new Ui::create_account_window)
 {
-    ui->setupUi(this);
+    m_p_ui->setupUi(this);
 }
 
 create_account_window::~create_account_window()
 {
-    delete ui;
+    delete m_p_ui;
 }
