89a5bdc7d0f9bf6e0d030ac7491e0c76c5384eb5
Hunter L. Allen <allen286@purdue.edu>
2016-10-28 04:57:32 -0400

    Made the create an account window open when the button is pressed, as well as made the window close upon either cancel or create. Next step would be to fill in the handlers in create_account_window.cpp with the appropriate action for each. Cancel is probably done, but the create button should send data to the server.

--- create_account_window.hpp
+++ create_account_window.hpp
@@ -19,7 +19,11 @@ public:
    explicit create_account_window (QWidget * _p_parent = NULL);
    virtual ~create_account_window();
 
+   Q_SLOT void create_pressed();
+   Q_SLOT void cancel_pressed();
+   
    Q_SIGNAL void send_create_account(user * u);
+   Q_SIGNAL void return_to_user_page();
 private:
     Ui::create_account_window * m_p_ui;
 };
