b48af29b152caca57918be5a2a8df43bd13faa04
Ana-Gabriel <ana.gabriel2012@gmail.com>
2016-11-07 23:42:23 -0500

    linked available windows to home screen buttons

--- home_screen.hpp
+++ home_screen.hpp
@@ -1,9 +1,12 @@
 #ifndef HOME_SCREEN_HPP
 #define HOME_SCREEN_HPP
-
+#pragma once
 #include <QWidget>
 #include <QtGui>
 
+#include "ui_home_screen.h"
+#include "schedulinggrid.h"
+#include "createevent.h"
 
 namespace Ui {
 class home_screen;
@@ -17,8 +20,23 @@ public:
     explicit home_screen(QWidget *_p_parent = NULL);
     virtual ~home_screen();
 
+	Q_SLOT void to_account_settings();
+	Q_SLOT void to_see_schedule();
+	Q_SLOT void to_manage_groups();
+	Q_SLOT void to_create_event();
+
+	Q_SLOT void from_create_event();
+	Q_SLOT void from_see_schedule();
+	Q_SLOT void from_manage_groups();
+	Q_SLOT void from_account_settings();
 private:
     Ui::home_screen *m_p_ui;
+
+	QString * m_p_username;
+	QString * m_p_password;
+
+	schedulingGrid * m_p_schedule;
+	createevent * m_p_create_event;
 };
 
 #endif // HOME_SCREEN_H
