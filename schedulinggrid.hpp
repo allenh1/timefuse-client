9edc231fbef0a05b221bb8ee4650a254b46a95ef
Hunter L. Allen <hunter@openrobotics.org>
2017-12-30 19:11:20 -0600

    Spelled @anqi-wang's name correctly.

--- schedulinggrid.hpp
+++ schedulinggrid.hpp
@@ -1,4 +1,4 @@
-// Copyright 2017 Hunter L. Allen, Angie Wang
+// Copyright 2017 Hunter L. Allen, Anqi Wang
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
